1aff674d6ebb71f44dafdb31bb18c00a2379a16a
EL7ANAFY <153466248+El7ANAFY-1337@users.noreply.github.com>
2024-07-22 16:19:03 +0000

    modified:   "EL7anafy/Section 3 - JavaScript Fundamentals \342\200\223 Part 2/index.html" 	deleted:    "EL7anafy/Section 3 - JavaScript Fundamentals \342\200\223 Part 2/jsFiles/Assignments/Assignment_Functions.js" 	new file:   "EL7anafy/Section 3 - JavaScript Fundamentals \342\200\223 Part 2/jsFiles/Assignments/Lecture_1.js" 	modified:   "EL7anafy/Section 3 - JavaScript Fundamentals \342\200\223 Part 2/jsFiles/Challenges/Challenge_1.js"

--- EL7anafy/Section 3 - JavaScript Fundamentals – Part 2/jsFiles/Challenges/Challenge_1.js
+++ EL7anafy/Section 3 - JavaScript Fundamentals – Part 2/jsFiles/Challenges/Challenge_1.js
@@ -1,6 +1,6 @@
 "use strict";
-console.log(`${ageCaluc(100)}`);
+// console.log(`${ageCaluc(100)}`);
 
-function ageCaluc(birthYear) {
-  return 2024 - birthYear;
-}
+// function ageCaluc(birthYear) {
+//   return 2024 - birthYear;
+// }
