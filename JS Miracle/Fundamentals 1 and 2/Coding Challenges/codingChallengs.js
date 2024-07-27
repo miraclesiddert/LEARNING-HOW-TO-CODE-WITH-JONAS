f5038c3a48ebbe5ba594e035b00652b030de6df5
Seven <sevensasylum@gmail.com>
2024-07-26 21:50:03 -0400

    Updating (#5)
    Seven_Directory_Structured

---
+++ JS Miracle/Fundamentals 1 and 2/Coding Challenges/codingChallengs.js
@@ -0,0 +1,155 @@
+/*
+Coding Challenge #1
+Mark and John are trying to compare their BMI (Body Mass Index), which is 
+calculated using the formula:
+BMI = mass / height ** 2 = mass / (height * height) (mass in kg 
+and height in meter).
+Your tasks:
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both 
+versions)
+3. Create a Boolean variable 'markHigherBMI' containing information about 
+whether Mark has a higher BMI than John.
+Test data:
+§ Data 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 
+m tall.
+§ Data 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 
+m tall.
+GOOD LUCK � 
+*/
+
+// //test data 1 for mark
+// const markHeight = 1.69;
+// const markMass = 78;
+
+// //test data 1 for john
+// const johnHeight = 1.95;
+// const johnMass = 92;
+
+//test data 2 for mark
+// const markHeight = 1.88;
+// const markMass = 95;
+
+// //test data 2 for john
+// const johnHeight = 1.76;
+// const johnMass = 85;
+
+// const BHIMark = markMass / markHeight ** 2;
+// const BHIJohn = johnMass / (johnHeight * johnHeight);
+// const markhigherBHI = BHIMark > BHIJohn;
+// const johnHigherBMI = BHIJohn > BHIMark;
+// console.log(markhigherBHI);
+// console.log(johnHigherBMI);
+// console.log(BHIMark, BHIJohn);
+
+
+/*
+Coding Challenge #2
+Use the BMI example from Challenge #1, and the code you already wrote, and 
+improve it.
+Your tasks:
+1. Print a nice output to the console, saying who has the higher BMI. The message 
+is either "Mark's BMI is higher than John's!" or "John's BMI is higher than Mark's!"
+2. Use a template literal to include the BMI values in the outputs. Example: "Mark's
+BMI (28.3) is higher than John's (23.9)!"
+Hint: Use an if/else statement �
+GOOD LUCK � 
+*/
+
+// step one solution
+// if (BHIMark > BHIJohn) {
+//     console.log(`Mark's BMI is higher than John's!`);
+// } else {
+//     console.log(`John's BMI is higher than Mark's!`);
+// }
+
+//step 2 solution
+// const markBHI = markMass / markHeight;
+// const johnBHI = johnMass / johnHeight;
+// console.log(markhigherBHI, johnBHI); // just want to see how it look before implementing it in step two
+// if (BHIMark > BHIJohn) {
+//     console.log(`Mark's BMI (${markBHI}) is higher than John's!`);
+// } else {
+//     console.log(`John's BMI is higher than Mark's (${markBHI})!`);
+// }
+
+
+/*
+Coding Challenge #3
+There are two gymnastics teams, Dolphins and Koalas. They compete against each 
+other 3 times. The winner with the highest average score wins a trophy!
+Your tasks:
+1. Calculate the average score for each team, using the test data below
+2. Compare the team's average scores to determine the winner of the competition, 
+and print it to the console. Don't forget that there can be a draw, so test for that 
+as well (draw means they have the same average score)
+3. Bonus 1: Include a requirement for a minimum score of 100. With this rule, a 
+team only wins if it has a higher score than the other team, and the same time a 
+score of at least 100 points. Hint: Use a logical operator to test for minimum 
+score, as well as multiple else-if blocks �
+4. Bonus 2: Minimum score also applies to a draw! So a draw only happens when 
+both teams have the same score and both have a score greater or equal 100 
+points. Otherwise, no team wins the trophy
+Test data:
+§ Data 1: Dolphins score 96, 108 and 89. Koalas score 88, 91 and 110
+§ Data Bonus 1: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 123
+§ Data Bonus 2: Dolphins score 97, 112 and 101. Koalas score 109, 95 and 106
+GOOD LUCK 
+*/
+
+
+// // Test dolphins
+// const dolphinsFirstScore = 96;
+// const dolphinsSecondScore = 108;
+// const dolphinsThirdScore = 89;
+
+// //Test koalas
+// const koalasFristScore = 88;
+// const koalasSecondScore = 91;
+// const koalasThirdScore = 110;
+
+// // Calculating the average score for each team,
+// const dolphineAverage = (dolphinsFirstScore + dolphinsSecondScore + dolphinsThirdScore) / 3;
+// console.log(`Dolphins Average score is ${dolphineAverage}`);
+// const koalasAverage = (koalasFristScore + koalasSecondScore + koalasThirdScore) / 3;
+// console.log(`koalas Average score is ${koalasAverage}`);
+
+// //Comparing the team's average scores to determine the winner of the competition
+// if (dolphineAverage > koalasAverage) {
+//     console.log(`The winner of the competition is Dolphins`);
+// } else if (koalasAverage > dolphineAverage) {
+//     console.log(`The winner of the competition is Dolphins`);
+// } else {
+//     console.log(`The Game was a draw no one won!`);
+// }
+
+
+// // Bonus 1
+
+// // Test dolphins
+// const dolphinsFirstScore = 97;
+// const dolphinsSecondScore = 112;
+// const dolphinsThirdScore = 101;
+
+// //Test koalas
+// const koalasFristScore = 109;
+// const koalasSecondScore = 95;
+// const koalasThirdScore = 106;
+
+// // Calculating the average score for each team,
+// const dolphineAverage = (dolphinsFirstScore + dolphinsSecondScore + dolphinsThirdScore) / 3;
+// console.log(`Dolphins Average score is ${dolphineAverage}`);
+// const koalasAverage = (koalasFristScore + koalasSecondScore + koalasThirdScore) / 3;
+// console.log(`koalas Average score is ${koalasAverage}`);
+
+// //Comparing the team's average scores to determine the winner of the competition
+// if (dolphineAverage > koalasAverage && dolphineAverage === 100) {
+//     console.log(`The winner of the competition is Dolphins`);
+// } else if (koalasAverage > dolphineAverage && dolphineAverage ===100) {
+//     console.log(`The winner of the competition is Dolphins`);
+// } else {
+//     console.log(`The Game was a draw no one won!`);
+// }
+
+//
+
---
+++ JS Miracle/Fundamentals 1 and 2/Lectures/script.js
@@ -0,0 +1,254 @@
+
+// //
+
+
+// let miracleAge = 21;
+// console.log(miracleAge);
+
+// console.log(typeof miracleAge);
+// console.log(typeof true);
+
+// //maths operators
+// const now = 2024
+// const ageMiracle = now - 2002;
+// const AgeQueen = now - 2004;
+// console.log(ageMiracle, AgeQueen);
+
+// console.log(ageMiracle * 2, AgeQueen / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+// const firstName = 'miracle';
+// const lastName = 'pininee';
+// console.log(firstName + ' ' + lastName);
+
+
+// //Assignments operators which make the re
+// let x = 10 + 5; // 15
+// x += 10; //x = x +10 = 25
+// x *= 4; //x = x * 4 = 100
+// x++; //x = x + 1 increase the value by one
+// x--; //x = x - 1 decrease the value by one
+// console.log(x);
+
+//Comparison operators
+// console.log(ageMiracle > AgeQueen);
+// console.log(AgeQueen >= 18);
+
+// const isFUllAge = AgeQueen >= 18;
+// console.log(now - 2002 > now - 2004);
+
+// let x , y;
+// x = y = 25 - 10 - 5; // x = y = 10, x = 10
+// console.log(x, y);
+
+// const averageAge = (ageMiracle + AgeQueen) / 2;
+// console.log(ageMiracle, AgeQueen, averageAge);
+
+
+
+// const year = 2024;
+// const firstName = 'miracle';
+// const job = 'teacher';
+// const birthYear = '2002'
+// const currentYear = '2024';
+
+// const mimi = "I'm " + firstName + ', a ' + (birthYear - currentYear) + ' years old ' + job + '!.';
+// console.log(mimi);
+
+// const miracle = `I'm ${firstName}, a ${birthYear-currentYear} year old ${job}!.`;
+// console.log(miracle);
+
+// //STRING TEMPLATE LITERALS
+
+// const miracleNew = `i'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+//  console.log(miracleNew);
+
+
+// const firstName = 'miracle';
+// const job = 'teacher';
+// const birthYear = '2002'
+// const currentYear = '2024';
+
+// const mimi = "I'm " + firstName + ', a ' + (birthYear - currentYear) + ' years old ' + job + '!.';
+// console.log(mimi);
+
+// const miracle = `I'm ${firstName}, a ${birthYear-currentYear} year old ${job}!.`;
+// console.log(miracle);
+
+
+// console.log(`my name is string holder....just a regular string nothing much...`);
+
+// console.log(`strings with \n\
+// multiple\n\
+// line`);
+
+// console.log(`STRINGS
+// multiple
+// LINE`);
+
+
+
+
+
+//A PROGRAM WITH CHECKS WEATHER A PERSON IS ALLOWED TO START TAKING A DRIVER'S LICENSE OR NOT AND
+//IF SHE IS IT WILL PRINT THAT TO THE CONSOLE
+//IF NOT IT WILL PRINT HOW MANY YEARS ARE LEFT
+//UNTIL THE PERSON CAN START TAKINGTHE DRIVING LICENSE
+
+/*this is called an if else control structure
+if () {
+    
+} else {
+    
+}
+*/
+
+// const age = 13;
+
+// if (age >= 18) {
+//     console.log(`Sarah can start driving license 🚗🚗`);
+// } else {
+
+//     const yearsLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+// }
+
+
+// // this code contain the century which a person is born if is 2015 then it will be the 21 century
+// const birthyear = 1995;
+// let century; //define it outside the code block and reassign it in the code block
+
+// if (birthyear <= 2000) {
+//     century = 20; // if it is here it will not work inside the code block
+
+//     const yearLeft = 18 - age;
+//     console.log(`Sarah is too young. Wait another ${yearLeft} years :)`);
+// }
+
+
+// const birthyear = 1995;
+// let century;
+
+// if (birthyear <= 2000) {
+//     century = 20;
+// } else {
+//     century = 21;
+// }
+// console.log(century);
+
+//TYPE CONVERSION AND TYPE COERCION
+
+// //TYPE CONVERSION
+// const birthyear = '1995';
+// console.log(birthyear + 22);
+// console.log(Number(birthyear) + 22);
+// console.log(Number(birthyear)); // this operation turn the string to a number Needs to start with a capital letter N
+// console.log(String(23), 'years old'); // Must be capital S not s
+
+// //TYPE COERCION
+// console.log(" i'm a " + 23 + " years old developer");
+// console.log('23' - '10' - 3);
+// console.log('23' * '10');
+// console.log('23' / '10');
+
+// // =============  5 falsy valie : 0, '' , undefined, null, NaN.  ================
+
+// console.log(Boolean(0));
+// console.log(Boolean(undefined));
+// console.log(Boolean('miracle'));
+// console.log(Boolean({}));
+// console.log(Boolean(''));
+
+// // JAVASCRIPT DO TYPE COERCION WHEN USING LOGICAL operation OR LOGICAL CONTEXT LIKE IF/ELSE STETAMENT
+// const money = 0; //falsy value
+// if (money) {
+//     console.log(`Don't spend it all`);
+// } else {
+//     console.log(`You should get a job!`)
+// }
+
+// // how to test if something if define or not
+// let height; //falsy value but if the heightis is assign a value 0 height = 0; still a falsy value
+// if (height) {
+//     console.log(`YAY! height is defined`);
+// } else {
+//     console.log(`height is UNDEFINED.`);
+// }
+
+// const age = 18;
+// if (age === 18 ) console.log(`you just became an adult :D (strict)`);
+// if (age == 18) console.log(`you just became an adult :D(loose)`);
+// const favourite = Number(prompt(`what's your favourite number from 1 - 10 ?`));
+// alert(favourite);
+// console.log(fovourite);
+// console.log(typeof favourite);
+
+// if (favourite === 1) {//23 ===23
+//     alert(`cool! 1 is an amazing number!`);
+// } else if (favourite === 2) {
+//     console.log(`1 is a cool number`);
+// } else if (favourite === 3) {
+//     alert(`cool! 3 is an amazing number!`);
+// } else if (favourite === 4) {
+//     alert(`cool! 4 is an amazing number!`);
+// } else if (favourite === 5) {
+//     alert(`cool! 5 is an amazing number!`);
+// } else if (favourite === 6) {
+//     alert(`cool! 6 is an amazing number!`);
+// } else if (favourite === 7) {
+//     alert(`cool! 7 is an amazing number!`);
+// } else if (favourite === 8) {
+//     alert(`cool! 8 is an amazing number!`);
+// } else if (favourite === 9) {
+//     alert(`cool! 9 is an amazing number!`);
+// } else if (favourite === 10) {
+//     alert(`cool! 10 is an amazing number!`);
+// } else {
+//     alert(`All Numbers Are Colll!`)
+// }
+
+// //different operators
+// if (favourite !== 11) {
+//     prompt(`so what is your foviourite number ?`)
+// }
+
+// const hasDriversLicense = true;//A
+// const hasGoodVision = true;
+
+// console.log(hasDriversLicense && hasGoodVision);// AND Operator
+// console.log(hasDriversLicense || hasGoodVision);//OR Operator
+// console.log(!hasDriversLicense)// NOT Operator
+
+// if (hasDriversLicense && hasGoodVision) {
+//     console.log(`Miracle is abole to drive!`);
+// } else {
+//     console.log(`Someone else should drive...`);
+// }
+
+// const isTired = true; //c
+// console.log(hasDriversLicense && hasGoodVision && isTired);//OR Operator
+
+
+// // THIS CODE WILL ALLOWE MIRACLE TO DRIVE IF HE HAS A DRIVERS LICENSE, A GOOD VISION AND IF HE'S NOT isTired
+
+// if (hasDriversLicense && hasGoodVision && !isTired) {
+//     console.log(`Miracle is abole to drive!`);
+// } else {
+//     console.log(`Someone else should drive...`);
+// }
+
+const day = 'Monday';
+
+switch (day) {
+    case 'monday':// day ==='manday'
+        console.log(`Go to work by 7`);
+        console.log(`After work start coding`);
+        break
+    case 'tuesday':
+        console.log(`Watch theory videos before going to work`);
+        console.log(`go for center meeting by 5pm `);
+        break
+    case 'wednesday':
+        console.log(``);
+    
+}
