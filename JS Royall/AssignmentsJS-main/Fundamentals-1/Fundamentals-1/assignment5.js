62bb27be9ac8f1df0e005e0c9ac92dd136d876ef
Seven <sevensasylum@gmail.com>
2024-07-26 21:12:51 -0400

    Delete JS Royall/AssignmentsJS-main/Fundamentals-1 directory

--- JS Royall/AssignmentsJS-main/Fundamentals-1/Fundamentals-1/assignment5.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-//Strings and Template Literals
-
-/*
-1. Recreate the description variable from the last assignment, this time using the template literal syntax.
- */
-
-// const description = `${country} is in ${continent}, and its ${population} million people speak
-// ${language}.`;
